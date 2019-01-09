68210a57603e6d42d28ac195033c244ce1c42e82
rfargo <renaldy_fargo@yahoo.com>
2019-01-10 01:00:52 +0700

    get separate characters done

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from scipy.misc import imsave, imread, imresize
 import numpy as np
 from keras import backend as K
 from keras.models import model_from_yaml
+import cv2
 import re
 import base64
 import pickle
@@ -18,6 +19,21 @@ def load_model():
     model.load_weights('bin/model.h5')
     print('done')
     return model
+
+def separate_characters(image):
+    img = cv2.imread(image)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    _, contours, _ = cv2.findContours(gray.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    list = []
+    for i in range(len(contours)):
+        x,y,w,h = cv2.boundingRect(contours[i])
+        letter = img[y:y + h, x:x + w]
+        cv2.imwrite(str(i) + '.png', letter)
+        list.append(i)
+
+    cv2.destroyAllWindows()
+    return list
 
 @app.before_first_request
 def initialize():
@@ -41,6 +57,7 @@ def predict():
     x = imread('output.png', mode='L')
     x = np.invert(x)
     imsave('resized.png', x)
+    list = separate_characters('resized.png')
 
     x = imresize(x,(28,28))
     x = x.reshape(1,28,28,1)
@@ -53,7 +70,7 @@ def predict():
                 'confidence': str(max(out[0]) * 100)[:6]}
 
     K.clear_session()
-    return jsonify(response)
+    return True
 
 if __name__ == '__main__':
     app.run(debug=True)
